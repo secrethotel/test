90945a546281f44c1bfa0d28bd9a28c13de9b6f3
lx <lixiang@mugeda.com>
2014-11-13 14:38:23 +0800

    add seajs

---
+++ overlay.js
@@ -0,0 +1,23 @@
+define(function(require, exports, module) {
+  // 模块代码
+  var elementCreate = require("./elementCreate");
+  var overlay = (function(){
+  	var element = elementCreate.create("div", {
+  		styles: {
+  			display: "none",
+            width: "100%",
+            backgroundColor: "#000",
+            opacity: 0.35,
+            position: "absolute",
+            zIndex: 1,
+            left: 0,
+            top: 0,
+            bottom: 0
+  		}
+  	})
+  })
+
+
+
+  
+});
